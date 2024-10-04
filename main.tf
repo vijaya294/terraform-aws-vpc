eb4ac5afdcda8e3f786503c5400e4cee1c53f869
Srilatha <koduri0311@gmail.com>
2024-10-04 14:31:09 +0100

    commit

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true 
   tags = merge(
     var.common_tags,
     var.public_subnet_tags,
