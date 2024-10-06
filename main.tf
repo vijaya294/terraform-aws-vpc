ba1aa9f7828b8e2657c645edf911ad955da46285
Srilatha <koduri0311@gmail.com>
2024-10-06 17:03:04 +0100

    expense-infra-dev

--- main.tf
+++ main.tf
@@ -72,6 +72,20 @@ resource "aws_subnet" "database" {
   
 }
 
+#DB subnet group for RDS
+resource "aws_db_subnet_group" "default" {
+  name        = local.resource_name
+  subnet_ids  = aws_subnet.database[*].id
+
+  tags = merge(
+    var.common_tags,
+    var.db_subnet_group_tags,
+    {
+        Name = local.resource_name
+    }
+  ) 
+}
+
 resource "aws_eip" "nat" {
   domain = "vpc"
 }
--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,11 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id
 }
+
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
+
 /* output "az_zones" {
     value = data.aws_availability_zones.available
 }
