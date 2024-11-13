4d4e160ba6a2d08c760d5c69cdc6c71330c4a2e5
Srilatha <koduri0311@gmail.com>
2024-11-13 13:29:14 +0000

    terraform

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+module "vpc" {
+    #source = "../terraform-aws-vpc"
+    source = "https://github.com/vijaya294/terraform-aws-vpc.git?ref=main"
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = var.is_peering_required
+}
